9aff34ee4380b73e5212362447c7e3e921008dd4
astrielov <krendelllllll@gmail.com>
2018-10-12 17:38:01 +0300

    asdf

--- scripts/02.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt update >> /var/log/update_script.log && sudo apt upgrade -y >> /var/log/update_script.log
-# echo "0 0 * * 1 ~/path_to_script/02.sh" | crontab
--- scripts/03.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ls -lS | grep "^-" | awk '{print $5,$9}' | column -t
